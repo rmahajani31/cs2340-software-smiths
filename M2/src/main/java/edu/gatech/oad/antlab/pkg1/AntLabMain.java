93cd904b9a57ed0a1e191918f1bdc220db130cec
Arber Muharemi <arber.muharemi@gatech.edu>
2016-09-07 19:39:28 -0400

    added personal gradle file and updated spacing in AntLabMain.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("P3");
 		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("Arber Muharemi");
-                  toPrint += p4.toString("amuharemi3");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Vijay");
-                  toPrint += p5.toString("vupadhya6");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Arber Muharemi");
+          toPrint += p4.toString("amuharemi3");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Vijay");
+          toPrint += p5.toString("vupadhya6");
 
         System.out.println(toPrint);
 
